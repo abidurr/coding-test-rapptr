65ac218cbdd76b54e9f04fdc13c5cd56ec61c01a
abidurr <42abid@gmail.com>
2020-05-15 01:47:50 -0400

    Font styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
+    Hi
     </div>
   );
 }
-
-export default App;
